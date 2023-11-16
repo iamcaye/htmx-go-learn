9cc289ab02e31676b63b507a11af995629219a53
iamcaye <cayetano.biehler@gmail.com>
2023-11-16 23:11:41 +0100

    oob usage

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-    "html/template"
-    "io"
-    "github.com/labstack/echo/v4"
-    "github.com/labstack/echo/v4/middleware"
+	"html/template"
+	"io"
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
@@ -21,24 +23,57 @@ func newTemplate() *Templates {
     }
 }
 
-type Count struct {
-    Count int
+type Contact struct {
+    Name string
+    Email string
+}
+
+func newContact(name, email string) Contact {
+    return Contact{
+        Name: name,
+        Email: email,
+    }
+}
+
+type Contacts []Contact
+
+type Data struct {
+    Contacts Contacts
+}
+
+func newData() Data {
+    return Data{
+        Contacts: []Contact{
+            newContact("caye", "caye@caye.es"),
+            newContact("arnold", "arnold@arnold.es"),
+        },
+    }
 }
 
 func main () {
     e := echo.New()
     e.Use(middleware.Logger())
 
-    count := Count { Count: 0 }
+    data := newData()
+
     e.Renderer = newTemplate()
 
     e.GET("/", func(c echo.Context) error {
-        return c.Render(200, "index", count)
+        return c.Render(200, "index", data)
     })
 
-    e.POST("/count", func(c echo.Context) error {
-        count.Count++
-        return c.Render(200, "count", count)
+    e.POST("/add-contact", func(c echo.Context) error {
+        name := c.FormValue("name")
+        email := c.FormValue("email")
+
+        contact := newContact(name, email)
+        data.Contacts = append(data.Contacts, contact)
+
+        err := c.Render(200, "contact-form", data);
+        if (err != nil) {
+            log.Fatal(err)
+        }
+        return c.Render(200, "oob-contact-item", contact)
     })
 
     e.Logger.Fatal(e.Start(":3000"))
