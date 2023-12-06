05db12a6dede98caf8fa226d445e028bbe23ae1c
iamcaye <cayetano.biehler@gmail.com>
2023-12-06 21:06:49 +0100

    post list

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,13 @@ type Templates struct {
     templates *template.Template
 }
 
+type Post struct {
+    Title string
+    Body  string
+}
+
+type Posts []Post
+
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
     return t.templates.ExecuteTemplate(w, name, data)
 }
@@ -24,23 +31,51 @@ func newTemplate() *Templates {
     }
 }
 
+func newPost(title string, body string) Post {
+    return Post{title, body}
+}
+
+func newPosts() Posts {
+    return Posts{
+		newPost("Title 1", "Body 1"),
+		newPost("Title 2", "Body 2"),
+		newPost("Title 3", "Body 3"),
+	}
+}
+
+type Data struct {
+    Posts Posts
+}
+
+func newData() Data {
+    return Data{newPosts()}
+}
 
 func main () {
     e := echo.New()
     e.Use(middleware.Logger())
     e.Renderer = newTemplate()
+    data := newData()
 
     e.Static("/css", "css")
 
     e.GET("/", func(c echo.Context) error {
-        return c.Render(200, "index", nil)
+        return c.Render(200, "index", data)
     })
 
     e.POST("/markdown", func(c echo.Context) error {
-	text := c.FormValue("markdown")
+	text := c.FormValue("body")
 	parsed := markdown.ToHTML([]byte(text), nil, nil)
-	return c.HTML(200, string(parsed))
+	post := newPost("Title", string(parsed))
+	return c.HTML(200, post.Body)
 	// return c.Render(200, "markdown-content", string(parsed))
+    })
+
+    e.POST("/add-post", func(c echo.Context) error {
+	body := c.FormValue("body")
+	title := c.FormValue("title")
+	data.Posts = append(data.Posts, newPost(title, body))
+	return c.Render(200, "post-list", data)
     })
 
     e.Logger.Fatal(e.Start(":3000"))
